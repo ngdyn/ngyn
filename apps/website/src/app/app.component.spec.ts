ec63932f2686d9fe9e9756746a1c1c15809b04e7
Mateo Tibaquira <nestormateo@gmail.com>
2025-07-27 01:42:59 -0500

    fix: sync nx21 clean setup with esbuild

--- apps/website/src/app/app.component.spec.ts
+++ apps/website/src/app/app.component.spec.ts
@@ -20,7 +20,10 @@ describe('AppComponent', () => {
 
   xit(`should have as title 'website'`, () => {
     // const fixture = TestBed.createComponent(AppComponent);
-    // const app = fixture.componentInstance;
-    // expect(app.title).toEqual('website');
+    // fixture.detectChanges();
+    // const compiled = fixture.nativeElement as HTMLElement;
+    // expect(compiled.querySelector('h1')?.textContent).toContain(
+    //   'Welcome website'
+    // );
   });
 });
